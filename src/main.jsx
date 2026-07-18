c6ab5813bafb793646c7c8d76ea42f4b53a2f74e
Jeff2Earth <jeff2earth@gmail.com>
2026-07-18 09:07:17 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LotLedger from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LotLedger />
+  </React.StrictMode>
+);
